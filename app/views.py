10019eeec05d970479fee9222d4e0c305ce2789c
Ubuntu <ubuntu@ip-172-31-35-67.us-west-2.compute.internal>
2018-07-01 19:40:04 +0000

    Added colors for medal tally

--- app/views.py
+++ app/views.py
@@ -17,18 +17,21 @@ def data(request):
 	for row in rows:
 		json_data = json.loads(serializers.serialize('json', [row]))
 		json_data = json_data[0]
-		medals_tally.append(json_data['fields'])
+		country = json_data['pk']
+		json_data = json_data['fields']
+		json_data['country'] = country
+		medals_tally.append(json_data)
 
 	chart = {'title': {'text': 'Winter Olympics Medals', 'x': -20},
 			'subtitle': {'text': 'Source: wikipedia.org', 'x': -20},
 			'xAxis': {'categories': [ row.country for row in rows ]},
 			'yAxis': {'title': {'text': 'Medals'},
-			'plotLines': [{'value': 0, 'width': 1, 'color': '#808080'}]},
+			'plotLines': [{'value': 0, 'width': 1, 'color': '#108080'}]},
 			'legend': {'layout': 'vertical','align': 'right',
 				'verticalAlign': 'middle', 'borderWidth': 0	},
-			'series': [{'name': 'Gold',	'data': [ row.gold for row in rows ]},
-			{'name': 'Silver', 'data' : [ row.silver for row in rows ]},
-			{'name': 'Bronze', 'data': [ row.bronze for row in rows ]}]}
+			'series': [{'name': 'Gold', 'data': [ row.gold for row in rows ], 'color' : "#ffd700"},
+			{'name': 'Silver', 'data' : [ row.silver for row in rows ], 'color' : "#c0c0c0"},
+			{'name': 'Bronze', 'data': [ row.bronze for row in rows ], 'color' : "#cd7f32"}]}
 	output_data = {'chart' : chart, 'medals' : medals_tally}
 	return JsonResponse(output_data)
 
